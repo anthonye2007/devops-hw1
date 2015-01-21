a7cfb694978549b548b2a33847e9b1a603edb8d2
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-01-21 16:03:29 -0500

    Simplify

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 var fs                = require('fs');
 var computeManagement = require('azure-mgmt-compute');
 
-//console.log(computeManagement.createCertificateCloudCredentials);
-
-var pemFile = fs.readFileSync('azure.pem', 'utf-8');
-console.log("pemFile: " + pemFile);
-
 var credentials = {
   subscriptionId: '8f323954-73ce-46ad-bb2b-a7e563e4c572',
-  pem: pemFile
+  pem: fs.readFileSync('azure.pem', 'utf-8')
 };
 
-console.log("Credentials: " + credentials);
-
-var me = computeManagement.createCertificateCloudCredentials(credentials);
-
-var computeManagementClient = computeManagement.createComputeManagementClient(me);
-
+var computeManagementClient = computeManagement.createComputeManagementClient(computeManagement.createCertificateCloudCredentials(credentials));
